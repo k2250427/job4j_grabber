94da9b42040c7a717a6fe952afcf0f4e4c6cfdb5
Stasyuk.K <kstasyuk@ya.ru>
2021-08-04 14:57:16 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/Post.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+}
