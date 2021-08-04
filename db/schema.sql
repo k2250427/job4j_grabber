d6d369ef51d99e37223ebe2c6c5106908dca8100
CS <kstasyuk@ya.ru>
2021-08-04 23:49:57 +0300

    4. Схема таблицы Post [#1731]

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 create table if not exists rabbit (
     created_date timestamp
 );
+
+create table if not exists post (
+    id serial primary key,
+    name varchar(100),
+    text text,
+    link varchar(255) unique,
+    created timestamp
+);
